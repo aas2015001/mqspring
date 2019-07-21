ee04fe45619c3d1dde68d17117deb475dd81be99
aas2015001 <aas2015001@gmail.com>
2019-07-21 14:03:01 -0400

    mq spring example

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a java project repository for using IBM MQ library
 
 * https://developer.ibm.com/tutorials/mq-jms-application-development-with-spring-boot/
 * https://start.spring.io/
+* https://mvnrepository.com/artifact/com.ibm.mq/mq-jms-spring-boot-starter/2.1.2
--- src/main/java/com/example/mqspring/MqspringApplication.java
+++ src/main/java/com/example/mqspring/MqspringApplication.java
@@ -1,13 +1,47 @@
 package com.example.mqspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableJms
 public class MqspringApplication {
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MqspringApplication.class, args);
+	}
+
+	@GetMapping("send")
+	public String send() {
+		try {
+			jmsTemplate.convertAndSend("DEV.QUEUE.1", "Hello World!");
+			return "Ok";
+		}
+		catch (JmsException ex) {
+			ex.printStackTrace();
+			return "Failed";
+		}
+	}
+
+	@GetMapping("recv")
+	public String recv() {
+		try {
+			return jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+		}
+		catch (JmsException ex) {
+			ex.printStackTrace();
+			return "Failed";
+		}
 	}
 
 }
